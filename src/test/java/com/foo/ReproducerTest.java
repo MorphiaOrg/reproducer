be783b363f896039d8a72469019d2e5092dbad73
evanchooly <jlee@antwerkz.com>
2020-12-09 20:29:14 -0500

    update to use bottlerocket include both 2.0 and 1.6 artifacts in the pom

--- src/test/java/com/foo/ReproducerTest.java
+++ src/test/java/com/foo/ReproducerTest.java
@@ -1,10 +1,40 @@
 package com.foo;
 
-import dev.morphia.TestBase;
+import com.antwerkz.bottlerocket.BottleRocket;
+import com.antwerkz.bottlerocket.BottleRocketTest;
+import com.github.zafarkhaja.semver.Version;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
-public class ReproducerTest extends TestBase {
+public class ReproducerTest extends BottleRocketTest {
+    private Datastore datastore;
+
+    ReproducerTest() {
+        MongoClient mongo = getMongoClient();
+        MongoDatabase database = getDatabase();
+        database.drop();
+        datastore = Morphia.createDatastore(mongo, getDatabase().getName());
+    }
+
+    @NotNull
+    @Override
+    public String databaseName() {
+        return "morphia_repro";
+    }
+
+    @Nullable
+    @Override
+    public Version version() {
+        return BottleRocket.DEFAULT_VERSION;
+    }
+
     @Test
     public void reproduce() {
     }
+
 }
