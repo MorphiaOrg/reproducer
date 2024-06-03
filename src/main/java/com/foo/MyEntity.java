09bd4061bca98ee1e375457276d011d5ed2e376a
Daniel Merchant <dmerchant@squarespace.com>
2024-06-03 17:52:20 -0400

    Broken with my enum

--- src/main/java/com/foo/MyEntity.java
+++ src/main/java/com/foo/MyEntity.java
@@ -1,7 +1,50 @@
 package com.foo;
 
-import dev.morphia.annotations.Entity;
+import java.util.Date;
+import java.util.Map;
 
-@Entity
+import org.bson.types.ObjectId;
+
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+
+@Entity("MyEntityCollection")
 public class MyEntity {
+  @Id
+  private ObjectId id;
+  private Map<MyEnum, String> values;
+  private Date updatedOn;
+  public MyEntity() {}
+
+  public ObjectId getId() {
+    return id;
+  }
+
+  public void setId(ObjectId id) {
+    this.id = id;
+  }
+
+  public Map<MyEnum, String> getValues() {
+    return values;
+  }
+
+  public void setValues(Map<MyEnum, String> values) {
+    this.values = values;
+  }
+
+  @Override
+  public String toString() {
+    return "MyEntity{" +
+        "id=" + id +
+        ", values=" + values +
+        '}';
+  }
+
+  public Date getUpdatedOn() {
+    return updatedOn;
+  }
+
+  public void setUpdatedOn(Date updatedOn) {
+    this.updatedOn = updatedOn;
+  }
 }
--- src/test/java/com/foo/ReproducerTest.java
+++ src/test/java/com/foo/ReproducerTest.java
@@ -1,5 +1,9 @@
 package com.foo;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.antwerkz.bottlerocket.BottleRocket;
 import com.antwerkz.bottlerocket.BottleRocketTest;
 import com.github.zafarkhaja.semver.Version;
@@ -7,6 +11,12 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
+import dev.morphia.mapping.MapperOptions;
+import dev.morphia.mapping.codec.Conversions;
+import dev.morphia.query.UpdateOperations;
+import dev.morphia.query.filters.Filters;
+
+import org.bson.types.ObjectId;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.testng.annotations.Test;
@@ -18,7 +28,23 @@ public class ReproducerTest extends BottleRocketTest {
         MongoClient mongo = getMongoClient();
         MongoDatabase database = getDatabase();
         database.drop();
-        datastore = Morphia.createDatastore(mongo, getDatabase().getName());
+        datastore = Morphia.createDatastore(mongo, getDatabase().getName(), MapperOptions.legacy().build());
+        Conversions.register(MyEnum.class, String.class, MyEnum::getKey);
+        Conversions.register(String.class, MyEnum.class, MyEnumConversionUtility::getFromKey);
+        datastore.getMapper().map(MyEntity.class);
+    }
+
+    private static final class MyEnumConversionUtility {
+        public static MyEnum getFromKey(String key) {
+            if (key.equals("first")) {
+                return MyEnum.FIRST;
+            } else if (key.equals("second")) {
+                return MyEnum.SECOND;
+            } else if (key.equals("third")) {
+                return MyEnum.THIRD;
+            }
+            throw new RuntimeException();
+        }
     }
 
     @NotNull
@@ -35,6 +61,30 @@ public class ReproducerTest extends BottleRocketTest {
 
     @Test
     public void reproduce() {
-    }
+        MyEntity entity = new MyEntity();
+        entity.setId(ObjectId.get());
+        Map<MyEnum, String> values = new HashMap<>();
+        values.put(MyEnum.FIRST, "first");
+        entity.setValues(values);
+        entity.setUpdatedOn(new Date());
+        datastore.save(entity);
 
+        System.out.println(datastore.find(MyEntity.class)
+            .filter("id", entity.getId())
+            .first());
+
+        Map<MyEnum, String> newValues = new HashMap<>();
+        newValues.put(MyEnum.SECOND, "second");
+
+        UpdateOperations<MyEntity> updateOps = datastore.createUpdateOperations(MyEntity.class)
+            .set("values", newValues)
+            .set("updatedOn", new Date());
+
+        datastore.update(datastore.find(MyEntity.class)
+            .filter("id", entity.getId()), updateOps);
+
+        System.out.println(datastore.find(MyEntity.class)
+            .filter("id", entity.getId())
+            .first());
+    }
 }
